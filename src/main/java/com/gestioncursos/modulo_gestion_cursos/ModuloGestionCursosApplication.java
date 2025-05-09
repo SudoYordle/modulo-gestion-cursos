9c87413ed4dbaa590eb6258bf65cf47f8fd25faf
Yordle <cri.collao@duocuc.cl>
2025-05-09 19:03:52 -0400

    first commit

---
+++ src/main/java/com/gestioncursos/modulo_gestion_cursos/ModuloGestionCursosApplication.java
@@ -0,0 +1,13 @@
+package com.gestioncursos.modulo_gestion_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloGestionCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloGestionCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestioncursos/modulo_gestion_cursos/ModuloGestionCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestioncursos.modulo_gestion_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloGestionCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
